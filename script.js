2a3498f8d5bb16a790019e85309b355ff1026e21
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-17 22:53:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
-
+const bandname=['The Beatles', 'Aerosmith', 'The Rolling Stones', 'Anthrax', 'The Who'];
+function strip(bandname){
+	return bandname.replace(^(a |the |an )/i, '').trim();
+	
+}
+const sortedband=bandnames.sort((a,b)=>strip(a)>strip(b)?1:-1);
+const bandlist=document.querySelector('#bands');
+for(let i=0; i<sortedband.length ; i++)
+	{
+		const li=document.createElement('li');
+	li.textContent=sortedband[li];
+	bandlist.appendChild(li);
+	}
